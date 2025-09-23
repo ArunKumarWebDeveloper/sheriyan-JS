d76fbbc94500e202da991250fb7483736f6a7225
Arun <arunlokesh78@gmail.com>
2025-09-23 13:30:46 +0530

    updated-codes

--- index.js
+++ index.js
@@ -40,3 +40,59 @@
 // for(let i=1; i<10; i++){
 //     console.log(i);
 // }
+//while loop//
+// let i=1;
+// while(i<=8){
+//     i++;
+//     console.log(i);
+// }
+//do while loop//
+// let i=1;
+// do{
+
+// console.log(i);
+// i++;
+// }
+// while(i<10);
+//break//
+// for(i=1; i<201; i++){
+//     console.log(i);
+//   if(i===32){
+//     break;
+//   }  
+// }
+
+//practice //
+//print numbers from 1 to 100//\
+
+// for (let a=1; a<=100; a++){
+//     console.log(a);
+// }
+
+// let i=10;
+// while(i>0){
+//     console.log(i);
+//     i--;
+// }
+
+// for (let i=2; i<=20; i+=2){
+// console.log(i);
+// }
+
+// for(let i=1; i<21; i++){
+//     if(i %2 === 0){
+//         console.log(i); 
+//     }
+// }
+// let i=1;
+// while(i<16){
+//     if(i%2 === 1){
+//         console.log(i);
+//     }
+//     i++;
+
+// }
+
+// for (let i=1; i<11; i++ ){
+// console.log(`5 * ${i} = ${5* i}`);
+// }
